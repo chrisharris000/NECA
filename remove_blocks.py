d22e885531b2bf06f7d10c4ca5b8c15a584bfc75
chrisharris000 <chris2145.ch@gmail.com>
2018-05-08 18:05:19 +1000

    update to remove_blocks and sample output

--- remove_blocks.py
+++ remove_blocks.py
@@ -8,20 +8,43 @@ Removes side blocks and other unecessary features from a moodle page
 
 document_title = ''
 
-#url = input("Enter path: ").replace('\\','\\\\')
+print("For example:\tC:\\Users\\harri\\Documents\\GitHub\\NECA\\Installation Testing.html")
+original_location = input("Enter path to orignal: ").replace('\\','\\\\')
+print("For example:\tC:\\Users\\harri\\Documents\\GitHub\\NECA\\")
+modified_location = input("Enter path for modified: ").replace('\\','\\\\')
 
-url = "C:\\Users\\harri\\Desktop\\Installation Testing.html"
-soup = BeautifulSoup(open(url),'html.parser')
+soup = BeautifulSoup(open(original_location),'html.parser')
 document_title = soup.title.get_text()
 
 #removes log out button
-
-#removes submenu
-for div in soup.find_all("div", {'class':'lb_submenu'}): 
+for div in soup.find_all("div", {"id":"page-header"}): 
     div.decompose()
 
-#removes 
+#removes submenu
+for div in soup.find_all("div", {"class":"lb_submenu"}): 
+    div.decompose()
+
+#removes side bar
+for div in soup.find_all("div", {"id":"block-region-side-pre"}):
+    div.decompose()
+
+'''#remove quiz stats (time, %, etc)
+
+#NOTE: Not working yet
+for div in soup.find_all("div", _class = "quizreviewsummary"):
+    div.decompose()
+
+#removes finish review button
+
+#NOTE: Not working yet
+for div in soup.find_all("div", {"id":"submitbtns"}):
+    div.decompose()'''
+
+#removes footer
+for div in soup.find_all("div", {"id":"page-footer"}):
+    div.decompose()
+
 html = soup.prettify("utf-8")
-output_file = document_title + ' - MODIFIED.html'
+output_file = modified_location + document_title + " - MODIFIED.html"
 with open(output_file, "wb") as file:
     file.write(html)
