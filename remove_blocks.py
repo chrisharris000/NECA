74d0e02df8d9c846226c8828cd443c6c60862549
chrisharris000 <chris2145.ch@gmail.com>
2018-05-09 16:21:03 +1000

    error checking

--- remove_blocks.py
+++ remove_blocks.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3.5
 # -*- coding: utf-8 -*-
 from bs4 import BeautifulSoup
-
+import sys
 
 '''
 Removes side blocks and other unecessary features from a moodle page
@@ -15,7 +15,11 @@ original_location = input("Enter path to orignal: ").replace('\\','\\\\')
 print("For example:\tC:\\Users\\harri\\Documents\\GitHub\\NECA\\")
 modified_location = input("Enter path for modified: ").replace('\\','\\\\')
 
-soup = BeautifulSoup(open(original_location),'html.parser')
+try:
+    soup = BeautifulSoup(open(original_location),'html.parser')
+except FileNotFoundError:
+    print("\n\nINVALID SOURCE FILE LOCATION\n\n")
+    sys.exit()
 document_title = soup.title.get_text()
 
 #removes log out button
@@ -40,6 +44,12 @@ for div in soup.find_all("div", {"id":"page-footer"}):
     div.decompose()
 
 html = soup.prettify("utf-8")
+
+
 output_file = modified_location + document_title + " - MODIFIED.html"
-with open(output_file, "wb") as file:
-    file.write(html)
+try:
+    with open(output_file, "wb") as file:
+        file.write(html)
+except FileNotFoundError:
+    print("INVALID FILE OUTPUT LOCATION")
+    sys.exit()
