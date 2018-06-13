4a3cfaea0379e3f30639d39dc25c38819988c471
chrisharris000 <chris2145.ch@gmail.com>
2018-06-13 20:59:15 +1000

    all scraping functions working
    don't know how it will perform when exported to excel though

--- uoc_scraping.py
+++ uoc_scraping.py
@@ -17,20 +17,37 @@ def scrape_document(uoc):
     assess_tool_ver = uoc + "_Assessment_Mapping_Toolv1.1"
     range_statement = ""
     asp_of_evidence = ""
-    elem_perf_criteria = []
-    req_skill_knowledge = []
+    elem_perf_criteria = ""
+    req_skill_knowledge = ""
 
     for e in soup.find_all("h2"):
         if e.string == "Range Statement":
-            print(e.string)
             range_table = e.find_next_siblings()[0]
-            extract_range_statement(range_table)
-            
-def extract_range_statement(table):
-    for table_row in table:
-        print(table_row.find("td"))
-        print("Done")
+            range_statement = extract_table_data(range_table)
 
+        if e.string == "Evidence Guide":
+            evidence_guide_table = e.find_next_siblings()[4]
+            asp_of_evidence = extract_table_data(evidence_guide_table)
+
+        if e.string == "Elements and Performance Criteria":
+            perf_criteria_table = e.find_next_siblings()[0]
+            data = (perf_criteria_table.find_all("tr"))
+            for i in range(len(data)):
+                elem_perf_criteria += data[i].get_text()
+
+        if e.string == "Required Skills and Knowledge":
+            k_and_s_table = e.find_next_siblings()[0]
+            data = (k_and_s_table.find_all("tr"))
+            for i in range(len(data)):
+                req_skill_knowledge += data[i].get_text()
+    print(req_skill_knowledge)
+
+
+def extract_table_data(table):
+    s = ''
+    for row in table.find_all("td"):
+        s += row.get_text()
+    return s
 
 uoc_names = ["UEENEEK142A"]
 document_title = ''
